fed77297d38d2dafd15d95fc428f1da1ddea35e2
ysf <josif.costin@gmail.com>
2018-05-11 11:22:41 +0300

    axsdcfv

--- src/Problema_21.java
+++ src/Problema_21.java
@@ -6,8 +6,24 @@ public class Problema_21 {
 
         int sir [] = {12, 44, 76, 29, 0, 49, 6, 127, 8, 22};
 
+        int nr1 = SkeletonJava.readIntConsole("Primul numar: ");
+        int nr2 = SkeletonJava.readIntConsole("Al doilea numar: ");
+
+        int sir2 [] = new int[sir.length];
+
+        System.out.println("Intre aceste valori sirul este: " );
+        for (int i=0; i<sir.length; i++) {
+            if ((sir[i]>=nr1 && sir[i]<=nr2) || (sir[i]<=nr1 && sir[i]>=nr2)) {
+
+                sir2[i] = sir[i];
+                }
+            System.out.println(sir2[i]);
+
+            }
+
+        }
 
 
 
     }
-}
+
